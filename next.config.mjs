3857fdffb843eb96d7083e542e924a73498528b5
tadzio <tadzio.holtrop@icloud.com>
2025-05-19 23:51:05 +0100

    udpate resume and add domains to nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "deims.org",
+      "media.springernature.com"
+      // add other domains if needed
+    ],
+  },
 };
 
 export default nextConfig;
